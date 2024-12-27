eb247c78b3948559a9e27822a7a75d5a3f45aa4e
muminovbob <bobomurodm2004@gmail.com>
2024-12-27 12:10:19 +0500

    custom block

--- src/main/java/net/bobosh/tutorialmod/TutorialMod.java
+++ src/main/java/net/bobosh/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.bobosh.tutorialmod;
 
+import net.bobosh.tutorialmod.block.ModBlocks;
 import net.bobosh.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -8,14 +9,11 @@ import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
